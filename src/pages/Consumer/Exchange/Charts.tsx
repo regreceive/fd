134c2ffbc9fa406a092c744ed9fa046dee8fa20e
liyao <regreceive@163.com>
2018-11-07 15:57:36 +0800

    实时电价

--- src/pages/Consumer/Exchange/Charts.tsx
+++ src/pages/Consumer/Exchange/Charts.tsx
@@ -25,19 +25,19 @@ export default class DoubleChart extends Component<IProps> {
       },
     };
     const scale = {
-      index: {
-        ticks: [
-          '1:00',
-          '4:00',
-          '7:00',
-          '10:00',
-          '13:00',
-          '16:00',
-          '19:00',
-          '22:00',
-        ],
-        tickCount: 8,
-      },
+      // index: {
+      //   ticks: [
+      //     '1:00',
+      //     '4:00',
+      //     '7:00',
+      //     '10:00',
+      //     '13:00',
+      //     '16:00',
+      //     '19:00',
+      //     '22:00',
+      //   ],
+      //   tickCount: 8,
+      // },
       price: {
         alias: translate('price'),
       },
@@ -71,13 +71,14 @@ export default class DoubleChart extends Component<IProps> {
           <Geom
             type="interval"
             position="index*actual"
-            color={'#0f00ff'}
+            color={'rgba(15,0,255,.5)'}
             size={7}
           />
+          <Geom type="point" position="index*price" size={2} color="#FE5816" />
           <Geom
             type="line"
             position="index*price"
-            size={5}
+            size={2}
             color="#FE5816"
             shape="smooth"
             style={{
